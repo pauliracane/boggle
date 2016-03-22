5447214b8aedf3c0791abbe1af033ca2e91b1089
pauliracane <smallmaddog11@yahoo.com>
2016-03-22 14:16:12 -0400

    Added comments to explain curses stuff.

--- boggle.py
+++ boggle.py
@@ -26,35 +26,36 @@ def main(stdscr):
 	guessedWords = []
 	points = 0
 	curses.echo()
+	
 	while time.time() < endTime and wordList:
 		stdscr.nodelay(True)
 		s = stdscr.getch()
 		if s != -1:
 			chr(s)
-			if s == 10:
-				for x in range(0,len(guess)):
+			if s == 10: # 10 is the carrage return sent by enter.
+				for x in range(0,len(guess)):		#Wipe the line
 					stdscr.addstr(" ")
-				stdscr.move(7,0)
-				if guess in wordList:
-					wordList.remove(guess)
-					wordlen = len(guess)
-					if wordlen <= 4:
-						points += 1
-					elif wordlen == 5:
-						points += 2
-					elif wordlen == 6:
-						points +=3
-					elif wordlen == 7:
-						points +=5
-					else:
-						points +=11
-					guessedWords.append(guess)
-					guess = ""
+				stdscr.move(7,0)			#move back to the start of the line
+				if guess in wordList:			#check if the guess is in the list of valid words
+					wordList.remove(guess)		#Remove it from the list
+					wordlen = len(guess)		#Get the length once, so it's less comparisons
+					if wordlen <= 4:		#If word is 3 or 4 letters
+						points += 1		#give 1 point
+					elif wordlen == 5:		#if 5 letters
+						points += 2		#give 2 points
+					elif wordlen == 6:		#if 6 letters
+						points +=3		#give 3 points
+					elif wordlen == 7:		#if 7 letters
+						points +=5		#give 5 poitns
+					else:				#if more than 7
+						points +=11		#give 11 points
+					guessedWords.append(guess)	#add the guess to a list of human guessed words
+					guess = ""			#reset guess to be null
 				else:
-					guess = ""
-			elif s == 263:
+					guess = ""			
+			elif s == 263:	#catch backspace character, remove last character passed in
 				guess = guess.replace(guess[-1],"")
-			elif s < 256:
+			elif s < 256:	#for each instance of a character being passed in.
 				guess+=str(chr(s))
 		
 
